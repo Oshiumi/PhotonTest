2c481326356fa2b1a036d1fd728d4596f1f3d2f8
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2015-09-29 04:28:03 +0900

    同期ができた

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   this.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, Input.GetAxisRaw("Vertical") * speed);
+	}
+}
